09f1de9dc884f349aff7bf0308796538cb9e54ec
andrewjohnso7 <andrewjohnso17@gmail.com>
2020-01-23 14:12:25 -0800

    updated gulpfile with autoprefixer info

--- project/gulpfile.js
+++ project/gulpfile.js
@@ -3,7 +3,7 @@ const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const browserSync = require('browser-sync');
-
+const autoprefixer = require('gulp-autoprefixer')
 // gulp.task('task-to-be-done', function() {
 //   // stuff here
 // });
@@ -19,6 +19,10 @@ gulp.task('sass', function() {
     .pipe(customPlumber('Error Running Sass'))
   // next how precise do you want decimals to be?
     .pipe(sass({precision: 2}))
+  // next it should autoprefix the compiled files for the varying vendors
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'] 
+    }))
   // next, where should it put the compiled files?
     .pipe(gulp.dest('app/css'))
   // next, tell browserSync to reload the files after the last task is done. 
@@ -33,7 +37,16 @@ gulp.task('browser', function() {
   browserSync({
     server: {
       baseDir: 'app',
-    }
+    },
+    open: false,
+    notify: false,
+    // NOTE: hella options exist inside of browser sync, 
+    // browser, allows you to specify which browsers to use in the development phase
+    // tunnel, allows you to specify a url for people who are not on your network to see what your working on
+    // open, allows you to specify which url you want to have browserSync load on.
+    // you can also disable browsers opening on default by passing false as an option.
+    // notify, will allow you to disable the default notification of browserSync on init.
+    // browser: ['google chrome', 'firefox']
   })
 });
 
